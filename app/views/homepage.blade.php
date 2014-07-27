2c41070a5a5ca1c8f773438ea13986c3c29c9e3c
Laura Filman <lmfilman@gmail.com>
2014-07-26 20:42:54 -0400

    added blurbs about the 2 tools to homepage

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -11,6 +11,14 @@
         <p class="lead">This application was written by Laura Filman as P3 for Harvard Extension's <a href="http://dwa15.com">Dynamic Web Applications</a> 2014 summer course.  
           The application contans two tools, a <em>Lorem Ipsum Generator</em> and a <em>User Generator</em>.
         </p>
+        <p>
+        	The <a href='/lorem-ipsum'>Lorem Ipsum Generator</a> is a tool which generates paragraphs of Latin text, which can be incorporated into a website during the design phase to 
+        	encourage developers and reviewers to focus on the visual design of a page and not have their attention drawn to the textual content.
+        </p>
+        <p>
+        	The <a href='/random-user'>User Generator</a> is a tool which can be used by developers to prepopulate their apps with fake user data, including names, birthdays, locations, and profile blurbs.
+        	Prepopulating an app with fake user data can be helpful for design and testing purposes.
+        </p>
       </div>
     </div>
 @stop
